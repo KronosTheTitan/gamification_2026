56d639198dad3b38a9e8cdc98c2fb9c3f03cb4fc
Tjalle Borgers <tjalle@borgersonline.nl>
2026-05-10 16:48:30 +0200

    setting up notification

--- Assets/user_interface.cs
+++ Assets/user_interface.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(UIDocument))]
 public class user_interface : MonoBehaviour
 {
+    [SerializeField] private UIDocument ui_document;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (ui_document == null)
+            ui_document = gameObject.GetComponent<UIDocument>();
     }
 }
