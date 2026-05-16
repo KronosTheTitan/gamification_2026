9a4e94acfb46d535bfe46144024444531f1263cc
Tjalle Borgers <tjalle@borgersonline.nl>
2026-05-16 17:44:10 +0200

    completed the map screen

--- Assets/user_interface.cs
+++ Assets/user_interface.cs
@@ -9,6 +9,9 @@ public class user_interface : MonoBehaviour
 
     private Button notification_button;
     private Button badge_menu_open_button;
+    private Button badge_menu_close_button;
+    private Button gallery_open_button;
+    private Button gallery_close_button;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,11 +23,37 @@ public class user_interface : MonoBehaviour
         
         notification_button = ui_document.rootVisualElement.Q<Button>("notification");
         badge_menu_open_button = ui_document.rootVisualElement.Q<Button>("badge_button_active");
+        badge_menu_close_button = ui_document.rootVisualElement.Q<Button>("back_button");
+
+        gallery_open_button = ui_document.rootVisualElement.Q<Button>("gallery_open_button");
+        gallery_close_button = ui_document.rootVisualElement.Q<Button>("gallery_close_button");
 
         notification_button.clicked += on_notification_clicked;
         badge_menu_open_button.clicked += on_badge_menu_open_button_clicked;
+        badge_menu_close_button.clicked += on_badge_menu_close_button_clicked;
+
+        gallery_open_button.clicked += on_gallery_open_button_clicked;
+        gallery_close_button.clicked += on_gallery_close_button_clicked;
     }
-    
+
+    private void on_gallery_open_button_clicked()
+    {
+        VisualElement gallery = ui_document.rootVisualElement.Q<VisualElement>("gallery_of_honor_menu");
+        VisualElement container = ui_document.rootVisualElement.Q<VisualElement>("badges_main_container");
+
+        container.style.display = DisplayStyle.None;
+        gallery.style.display = DisplayStyle.Flex;
+    }
+
+    private void on_gallery_close_button_clicked()
+    {
+        VisualElement gallery = ui_document.rootVisualElement.Q<VisualElement>("gallery_of_honor_menu");
+        VisualElement container = ui_document.rootVisualElement.Q<VisualElement>("badges_main_container");
+
+        container.style.display = DisplayStyle.Flex;
+        gallery.style.display = DisplayStyle.None;
+    }
+
     private void on_notification_clicked()
     {
         notification_button.visible = false;
@@ -32,12 +61,19 @@ public class user_interface : MonoBehaviour
 
     private void on_badge_menu_open_button_clicked()
     {
-        VisualElement main_container = ui_document.rootVisualElement.Q<VisualElement>("badges_main_container");
+        VisualElement gallery = ui_document.rootVisualElement.Q<VisualElement>("gallery_of_honor_menu");
         VisualElement badge_menu = ui_document.rootVisualElement.Q<VisualElement>("honorable_dogs_badge");
 
-        main_container.style.display = DisplayStyle.None;
+        gallery.style.display = DisplayStyle.None;
         badge_menu.style.display = DisplayStyle.Flex;
-        
-        
+    }
+
+    private void on_badge_menu_close_button_clicked()
+    {
+        VisualElement gallery = ui_document.rootVisualElement.Q<VisualElement>("gallery_of_honor_menu");
+        VisualElement badge_menu = ui_document.rootVisualElement.Q<VisualElement>("honorable_dogs_badge");
+
+        gallery.style.display = DisplayStyle.Flex;
+        badge_menu.style.display = DisplayStyle.None;
     }
 }
